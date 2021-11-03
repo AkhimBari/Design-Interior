cb60796587840da596f7acd8ea256b5607d4e547
AkhimBari <akhim.bari91@gmail.com>
2021-11-03 10:57:01 +0100

    first

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+let vorname = document.getElementById("js");
+let email = document.getElementById("email");
+let empty = document.getElementById("message");
+
+function button() {
+    console.log(vorname.value);
+    console.log(email.value);
+    console.log(empty.value);
+}
+
